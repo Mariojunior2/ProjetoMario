cbe205592a38f3b8effba9c4c5595898f8569768
Mariojunior2 <mario.junior2@alunos.sc.senac.br>
2024-10-06 00:10:53 -0300

    Audio ebas

--- assets/java/js.js
+++ assets/java/js.js
@@ -1,19 +1,38 @@
+const game = document.querySelector('.full');
+const start = document.querySelector('.Comeco');
+const startB = document.querySelector('.start');
+
+game.style.display = 'none';
+start.style.display = 'flex';
+
+function startI() {
+game.style.display = 'flex';
+start.style.display = 'none';
+const jumpAudio = new Audio('assets/audios/smw_jump.wav');
 const player = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const nuvem = document.querySelector('.cloud');
 
-
 const over = document.querySelector('.game-over');
-const restartB = document.querySelector('.restart')
+const restartB = document.querySelector('.restart');
+
+
+
 
 
 const jump = () => {
     player.classList.add('jump');
 
+    jumpAudio.currentTime = 0;
+        jumpAudio.play().catch(erro => {
+            console.error('Erro ao tocar o áudio:', erro);
+        });
     setTimeout(() => {
         player.classList.remove('jump');
     }, 500);
 }
+
+
 
 const circulo = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
@@ -43,8 +62,10 @@ const circulo = setInterval(() => {
 }, 10);
 
 
+
 const restart = () => {
     over.style.visibility = 'hidden';
+
     pipe.style.animation = 'pipe-animations 1.5s infinite linear';
     pipe.style.left = ``;
 
@@ -85,7 +106,39 @@ const restart = () => {
   }, 10);
 }
 
+
+
+
 document.addEventListener('keydown', jump);
 document.addEventListener('touchstart', jump);
 
 restartB.addEventListener('click', restart);
+
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.querySelector('.corP');
+    const text = textElement.textContent;
+    textElement.innerHTML = '';
+
+    for (let char of text) {
+        const span = document.createElement('span');
+        span.textContent = char;
+        textElement.appendChild(span);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.querySelector('.restartT');
+    const text = textElement.textContent;
+    textElement.innerHTML = '';
+
+    for (let char of text) {
+        const span = document.createElement('span');
+        span.textContent = char;
+        textElement.appendChild(span);
+    }
+});
